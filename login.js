75517c11de506ac1de459e02eae0ca31f588573f
isin5435 <isin5435@gmail.com>
2023-11-20 21:42:48 -0700

    login popup complete

---
+++ login.js
@@ -0,0 +1,13 @@
+function openPopup()
+    {
+        var popup = document.getElementById("popup");
+        popup.style.visibility = "visible";
+        popup.style.opacity = "1";
+    }
+
+    function closePopup()
+    {
+        var popup = document.getElementById("popup");
+        popup.style.visibility = "hidden";
+        popup.style.opacity = "0";
+    }
