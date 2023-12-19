d3e3a63f0510e4ae66713a3d739537b17d451031
isin5435 <isin5435@gmail.com>
2023-12-19 01:27:12 -0700

    navbar complete

---
+++ static/js/search.js
@@ -0,0 +1,13 @@
+function openwordPopup()
+    {
+        var popup = document.getElementById("wordPopup");
+        popup.style.visibility = "visible";
+        popup.style.opacity = "1";
+    }
+
+    function closewordPopup()
+    {
+        var popup = document.getElementById("wordPopup");
+        popup.style.visibility = "hidden";
+        popup.style.opacity = "0";
+    }
