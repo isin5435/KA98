c2fee5c19d205e254c18886aac394475372c2c12
isin5435 <isin5435@gmail.com>
2023-12-19 21:18:41 -0700

    userpage build comp

--- search/views.py
+++ search/views.py
@@ -8,7 +8,7 @@ def search_word(request):
     word = request.GET.get('word')
     result = Words.objects.filter(text=word).first()
     if result:
-        return JsonResponse({'word': result.text, 'meaning': result.definitions})
+        return JsonResponse({'word': result.text, 'meaning': result.definitions, 'show_button':True})
     
     response = requests.get(f'https://api.dictionaryapi.dev/api/v2/entries/en/{word}')
     data = response.json()
@@ -17,7 +17,7 @@ def search_word(request):
             meaning = item['meanings'][0]
             definitions = meaning['definitions'][0]
             definition = definitions['definition']
-            return JsonResponse({'word': word, 'meaning': definition})
+            return JsonResponse({'word': word, 'meaning': definition, 'show_button':True})
     else:
-        return JsonResponse({'word': "No word information in DataBase", 'meaning': ""})
+        return JsonResponse({'word': "No word information in DataBase", 'meaning': "", 'show_button':False})
 
--- static/js/search.js
+++ static/js/search.js
@@ -8,6 +8,25 @@ function openwordPopup()
             .then(response => response.json())
             .then(data => {
                 document.getElementById('wordInfo').innerHTML = `<h2>${data.word}</h2><p>${data.meaning}</p>`;
+                var button = document.getElementById('myButton');
+                if (data.show_button)
+                {
+                    if (!button)
+                    {
+                        button = document.createElement('button');
+                        button.id = 'myButton';
+                        button.innerText = 'add to my vocab';
+                        document.body.appendChild(button);
+                    }
+                    button.style.display = 'block';
+                }
+                else
+                {
+                    if (button)
+                    {
+                        button.style.display = 'none';
+                    }    
+                }
                 var popup = document.getElementById("wordPopup");
                 popup.style.visibility = "visible";
                 popup.style.opacity = "1";
